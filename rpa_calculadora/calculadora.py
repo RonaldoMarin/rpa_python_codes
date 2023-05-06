b204ae1cd9dfbc4aada699da699391b46b887609
Ronaldo Marinho <marinhoronaldo02@gmail.com>
2023-05-06 15:53:45 -0300

    update

--- rpa_calculadora/calculadora.py
+++ rpa_calculadora/calculadora.py
@@ -4,12 +4,12 @@ RPA para utilização da calculadora windows - v0.1
 
 import pyautogui
 
-pyautogui.press('win')
+pyautogui.press('win', interval=0.25)
 pyautogui.write('calculadora')
 pyautogui.press('enter', interval=1)
 
 
-pyautogui.hotkey('1', '1', '1', interval=0.25)
+pyautogui.hotkey('5', '5', '5', interval=0.25)
 pyautogui.press('*', interval=0.25)
 pyautogui.press('2', interval=0.25)
 pyautogui.press('enter')
