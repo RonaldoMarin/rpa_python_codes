78fcee7d1e9589dc28e4037de6eec38ba8d948e1
Ronaldo Marinho <marinhoronaldo02@gmail.com>
2023-05-06 16:57:42 -0300

    update

---
+++ pesquisa_yt/pesquisador.py
@@ -0,0 +1,24 @@
+'''
+Algorítmo para pesquisa de vídeos no youtube
+'''
+
+import pyautogui
+
+#Bloco para abrir o Navegador (google)
+pyautogui.press('win', interval=0.5)
+pyautogui.write('google')
+pyautogui.press('enter', interval=1.5)
+
+#Bloco abrir youtube
+pyautogui.write('Youtube.com')
+pyautogui.press('enter', interval=2)
+
+#Bloco fazer pesquisa
+#pyautogui.moveTo(568, 131)
+pyautogui.click(568, 131, interval=0.75)
+pyautogui.write('War on Babylon - Izrael Howls (Legendado)')
+pyautogui.press('enter', interval=2)
+
+#Bloco entrar no vídeo
+pyautogui.click(425, 336, interval=1)
+
--- rpa_calculadora/calculadora.py
+++ rpa_calculadora/calculadora.py
@@ -6,12 +6,12 @@ import pyautogui
 
 pyautogui.press('win', interval=0.25)
 pyautogui.write('calculadora')
-pyautogui.press('enter', interval=1)
+pyautogui.press('enter', interval=2)
 
 
 pyautogui.hotkey('5', '5', '5', interval=0.25)
-pyautogui.press('*', interval=0.25)
-pyautogui.press('2', interval=0.25)
+pyautogui.press('+', interval=0.25)
+pyautogui.press('5', interval=0.25)
 pyautogui.press('enter')
 
 
